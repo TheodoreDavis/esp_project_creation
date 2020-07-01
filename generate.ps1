6783edba51e365b454ad70673c06fcbf29c7ed46
tcdavis <tcdavis@iastate.edu>
2020-07-01 12:56:04 -0500

    Dialog is still broken but I added file creation that works!

--- generate.ps1
+++ generate.ps1
@@ -18,4 +18,22 @@ Switch ($temp) {
 	Default {Write-Host "Generating..."}
 }
 
-New-Item -ItemType "directory" -Path ""
+$name = "Sample_Project"
+$path = "C:\Users\stat_tcdavis\Desktop"
+
+$temp = $path + "\" + $name
+New-Item -Path $temp -Force -ItemType directory
+
+$path += "\" + $name
+
+$temp = $path + "\" + "CMakeLists.txt"
+Out-File -FilePath $temp -Encoding string -Force -InputObject "# Automatically generated using IDF_Project_Generator`ncmake_minimum_required(VERSION 3.5)`n`ninclude(`$ENV{IDF_PATH}/tools/cmake/project.cmake)`nproject($name)"
+$temp = $path + "\" + "Makefile"
+Out-File -FilePath $temp -Encoding string -Force -InputObject "# Automatically generated using IDF_Project_Generator`n`nPROJECT_NAME := $name`ninclude `$(IDF_PATH)/make/project.mk"
+
+$temp = $path + "\" + "main"
+New-Item -Path $temp -Force -ItemType directory
+
+$temp = $path + "\" + "main" + "\" + "CMakeLists.txt"
+Out-File -FilePath $temp -Encoding string -Force -InputObject "# Automatically generated using IDF_Project_Generator`nidf_component_register(SRCS `"$name.c`"`n`t`t`t`t`tINCLUDE_DIRS `".`")"
+
