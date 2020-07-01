9375a08667640d8cbda953622707ee7dfce875c5
tcdavis <TheDavis@users.noreply.github.com>
2020-06-30 22:44:48 -0500

    Well I broke it :(

--- generate.ps1
+++ generate.ps1
@@ -1,4 +1,21 @@
-Write-Host "THIS IS A TEST!"
-$Test = Read-Host "Type something"
-Write-Host "Did you say $Test?"
-Read-Host "Press enter to exit"
+&$prompts
+
+$prompts = {
+	$name = Read-Host "Name of your project"
+	$path = Read-Host "Path of your workspace"
+	cls
+	Write-Host "Is this information correct?"
+	Write-Host "Name: $name"
+	Write-Host "Path: $path"
+	.$prompts
+}
+
+$temp Read-Host "Is this information correct? [Y/n]"
+cls
+Switch ($temp) {
+	N {&$prompts}
+	n {&$prompts}
+	Default {Write-Host "Generating..."}
+}
+
+New-Item -ItemType "directory" -Path ""
